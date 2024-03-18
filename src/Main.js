1d842b885e73b50163e0661358a4ad0ef905048d
yosef <yoseftutian@gmail.com>
2024-03-18 15:22:49 +0200

    Comments in main file

--- src/Main.js
+++ src/Main.js
@@ -8,9 +8,9 @@ function Main() {
   useEffect(() => {
     axios
       .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef")
-
+      //You can change the name at the end of the link of the types of food
+      //and it will bring you a different list
       .then((res) => {
-        // console.log(res.data);
         setitems(res.data.meals);
       })
       .catch((err) => {
